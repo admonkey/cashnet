00859f1d91dc2f416bc7fc0615b8c2b9874b3383
Jeff Puckett <jeff@jeffpuckett.com>
2016-06-29 17:43:29 -0500

    create testSetPrice

---
+++ tests/CashnetTest.php
@@ -0,0 +1,13 @@
+<?php
+class CashnetTest extends PHPUnit_Framework_TestCase
+{
+  public function testSetPrice()
+  {
+    // Arrange
+
+    // Act
+
+    // Assert
+    $this->assertEquals(true, false);
+  }
+}
