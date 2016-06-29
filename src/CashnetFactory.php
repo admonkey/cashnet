12af39ebd04da0b11a32bcdbb3a997551f94e47c
Jeff Puckett <jeff@jeffpuckett.com>
2016-06-29 18:20:12 -0500

    create get and set price functions

--- src/CashnetFactory.php
+++ src/CashnetFactory.php
@@ -1,8 +1,35 @@
 <?php namespace Puckett\Cashnet;
 class CashnetFactory
 {
-  public function setPrice()
+
+/*
+  --
+  -- PUBLIC METHODS
+  --
+  -- functionName($parameters = DEFAULTS) returns
+  --
+
+  getPrice() numeric or false
+  setPrice($price) boolean
+
+*/
+
+  private $price;
+
+  function __construct()
   {
-    return false;
+    $this->price = false;
   }
+
+  public function getPrice()
+  {
+    return $this->price;
+  }
+
+  public function setPrice($price)
+  {
+    $this->price = $price;
+    return true;
+  }
+
 }
--- tests/CashnetTest.php
+++ tests/CashnetTest.php
@@ -5,11 +5,14 @@ class CashnetTest extends PHPUnit_Framework_TestCase
   {
     // Arrange
     $cf = new CashnetFactory();
+    $price = 42.42;
 
     // Act
-    $setPriceResponse = $cf->setPrice();
+    $setPriceResponse = $cf->setPrice($price);
+    $getPriceResponse = $cf->getPrice();
 
     // Assert
     $this->assertEquals(true, $setPriceResponse);
+    $this->assertEquals($price, $getPriceResponse);
   }
 }
