e574147f95e6c2d09a42a7387c78ebce9037b69d
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-06 18:47:21 -0500

    create DDL

---
+++ src/SQL/ddl.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS `cashnet_lineitems`;
+DROP TABLE IF EXISTS `cashnet_transactions`;
+
+CREATE TABLE IF NOT EXISTS `cashnet_transactions` (
+  `uid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `result` SMALLINT,
+  `respmessage` VARCHAR(255),
+  `itemcnt` SMALLINT,
+  `batchno` SMALLINT,
+  `tx` SMALLINT,
+  `cctype` VARCHAR(255),
+  `effdate` DATE NULL,
+  `failedtx` SMALLINT,
+  `ccerrorcode` SMALLINT,
+  `CARDNAME_G` VARCHAR(255),
+  `ADDR_G` VARCHAR(255),
+  `CITY_G` VARCHAR(255),
+  `STATE_G` VARCHAR(255),
+  `ZIP_G` VARCHAR(255),
+  `EMAIL_G` VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS `cashnet_transactions` (
+  `lid` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `tid` INT NOT NULL,
+  `itemcode` VARCHAR(255),
+  `amount` DECIMAL(15,2),
+  `qty` SMALLINT,
+  `gl` VARCHAR(255),
+  CONSTRAINT `fk_cashnet_lineitems` FOREIGN KEY (`tid`)
+  REFERENCES `cashnet_transactions`(`uid`)
+);
