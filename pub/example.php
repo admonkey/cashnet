0f46138500bb569dcd998c4e25bbc180979d8209
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-07 22:15:44 -0500

    use only MySQL PDO

--- pub/example.php
+++ pub/example.php
@@ -15,16 +15,6 @@ $database_username = 'tsc_UA_oO0cz';
 $database_password = 'R7h6sTJ3aBoXLqev';
 $database_name     = 'tsc_D_oO0cz';
 
-// some ODBC driver examples
-$mssql             = 'ODBC Driver 13 for SQL Server';
-$easysoft          = 'Easysoft ODBC-SQL Server';
-
-// PDO database connection string examples
-$mysql             = "mysql:host=$database_server;dbname=$database_name;charset=utf8";
-$odbc              = "odbc:Driver=$mssql;Server=$database_server;Database=$database_name";
-
-$pdo_database_connection_string = $mysql;
-
 // trump or modify with environment credentials
 $credentials_file = __DIR__.'/../credentials.local.inc.php';
 if (file_exists($credentials_file))
@@ -38,13 +28,17 @@ use Puckett\Cashnet\CashnetFactory;
 
 // set up your pdo database connection
 // http://php.net/manual/en/book.pdo.php
-$pdoDB = new PDO($pdo_database_connection_string, $database_username, $database_password);
+$pdoDB = new PDO(
+  "mysql:host=$database_server;dbname=$database_name;charset=utf8",
+  $database_username,
+  $database_password
+);
 
 // create object
 $cf = new CashnetFactory($data, $pdoDB);
-// $cf = new CashnetFactory($data);
 
+// get the rest of the fields
 $cf->getEmptyCashnetGlobals();
 
-// render form and handle post
+// render form, allow override, and handle post
 echo $cf->getForm(true);
